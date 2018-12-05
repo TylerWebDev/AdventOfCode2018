ed4c1d324d8bf7e28a5cd879939552e2d21b89dc
Logan Henson <logan@loganhenson.com>
2018-12-05 13:49:08 -0600

    D1P2 Elixir

--- Day1/ElixirLogan/lib/advent.ex
+++ Day1/ElixirLogan/lib/advent.ex
@@ -6,6 +6,17 @@ defmodule Advent do
   end
 
   def two(input) do
-    input
+    String.split(input, "\n")
+    |> Enum.map(&String.to_integer(&1))
+    |> Stream.cycle()
+    |> Enum.reduce_while({[0], 0}, fn number, {frequencies, frequency} ->
+      new_frequency = frequency + number
+
+      if Enum.member?(frequencies, new_frequency) do
+        {:halt, new_frequency}
+      else
+        {:cont, {[new_frequency | frequencies], new_frequency}}
+      end
+    end)
   end
 end
--- Day1/ElixirLogan/test/advent_test.exs
+++ Day1/ElixirLogan/test/advent_test.exs
@@ -6,4 +6,15 @@ defmodule AdventTest do
   test "one can sum the numbers properly" do
     assert Advent.one("+1\n-1\n+2\n-2") === 0
   end
+
+  # 2
+  test "two can find the repeated frequency ex1" do
+    # 0,1,0
+    assert Advent.two("+1\n-1") === 0
+  end
+
+  test "two can find the repeated frequency ex2" do
+    # 0,3,6,10,8,4,7,10
+    assert Advent.two("+3\n+3\n+4\n-2\n-4") === 10
+  end
 end
